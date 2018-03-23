4987e170ddf5d20c959e14751bcca2c43dd02d6c
Aaron Biser <abiser@aaronbiser.com>
2018-03-23 08:14:25 -0500

    updated readme and config file

--- README.md
+++ README.md
@@ -1,26 +1,32 @@
-# gatsby-starter-default
-The default Gatsby starter.
+# gatsby-wordpress-ui
+
+Gatsby front end UI using data from Wordpress backend. Created from the default Gatsby starter and gatsby-source-wordpress.
 
 For an overview of the project structure please refer to the [Gatsby documentation - Building with Components](https://www.gatsbyjs.org/docs/building-with-components/).
+
+For an overview of gatsby-source-wordpress refer to [gatsby-source-wordpress on GitHub](https://github.com/gatsbyjs/gatsby/tree/master/packages/gatsby-source-wordpress).
 
 ## Install
 
 Make sure that you have the Gatsby CLI program installed:
+
 ```sh
 npm install --global gatsby-cli
 ```
 
 And run from your CLI:
+
 ```sh
-gatsby new gatsby-example-site
+npm install
 ```
 
 Then you can run it by:
+
 ```sh
-cd gatsby-example-site
-npm run develop
+cd gatsby-wordpress-ui
+gatsby develop
 ```
 
-## Deploy
+## Wordpress Backend URL
 
-[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/gatsbyjs/gatsby-starter-default)
+Change the Wordpress URL in gatsby-config.js any new Wordpress install local or hosted.
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,50 +1,50 @@
 require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
+	path: `.env.${process.env.NODE_ENV}`,
 });
 
 module.exports = {
-  siteMetadata: {
-    title: 'The Right Place',
-    subtitle: 'Find the right bed for your patient',
-  },
-  plugins: [
-    'gatsby-plugin-sass',
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography.js`,
-      },
-    },
-    {
-      resolve: 'gatsby-source-wordpress',
-      options: {
-        /*
+	siteMetadata: {
+		title: 'The Right Place',
+		subtitle: 'Find the right bed for your patient',
+	},
+	plugins: [
+		'gatsby-plugin-sass',
+		'gatsby-plugin-react-helmet',
+		{
+			resolve: `gatsby-plugin-typography`,
+			options: {
+				pathToConfigModule: `src/utils/typography.js`,
+			},
+		},
+		{
+			resolve: 'gatsby-source-wordpress',
+			options: {
+				/*
       * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
       * Example : 'gatsbyjswpexample.wordpress.com' or 'www.example-site.com'
       */
-        baseUrl: process.env.API_URL,
-        // The protocol. This can be http or https.
-        protocol: 'http',
-        // Indicates whether the site is hosted on wordpress.com.
-        // If false, then the asumption is made that the site is self hosted.
-        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
-        // If your site is hosted on wordpress.org, then set this to false.
-        hostingWPCOM: false,
-        // If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
-        // This feature is untested for sites hosted on Wordpress.com
-        useACF: true,
-        auth: {
-          htaccess_user: process.env.AUTH_HTACCESS_USER,
-          htaccess_pass: process.env.AUTH_HTACCESS_PASSWORD,
-          htaccess_sendImmediately: true,
-          wpcom_app_clientSecret: '',
-          wpcom_app_clientId: '',
-          wpcom_user: '',
-          wpcom_pass: '',
-        },
-        verboseOutput: false,
-      },
-    },
-  ],
+				baseUrl: 'dev-gatbsyjswp.pantheonsite.io',
+				// The protocol. This can be http or https.
+				protocol: 'http',
+				// Indicates whether the site is hosted on wordpress.com.
+				// If false, then the asumption is made that the site is self hosted.
+				// If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
+				// If your site is hosted on wordpress.org, then set this to false.
+				hostingWPCOM: false,
+				// If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
+				// This feature is untested for sites hosted on Wordpress.com
+				useACF: true,
+				auth: {
+					htaccess_user: process.env.AUTH_HTACCESS_USER,
+					htaccess_pass: process.env.AUTH_HTACCESS_PASSWORD,
+					htaccess_sendImmediately: true,
+					wpcom_app_clientSecret: '',
+					wpcom_app_clientId: '',
+					wpcom_user: '',
+					wpcom_pass: '',
+				},
+				verboseOutput: false,
+			},
+		},
+	],
 };
